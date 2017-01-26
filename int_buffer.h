e001094a8e0783c72c193b336d46cc516bc7d769
stefa_000 <stefa_000@10.250.118.98>
2017-01-26 13:15:16 +0100

    new commit

---
+++ int_buffer.h
@@ -0,0 +1,36 @@
+#include <cstdlib>
+
+#ifndef INT_BUFFER_H
+#define INT_BUFFER_H
+
+class int_buffer {
+    int* start;
+    int* ending;
+public:
+    explicit int_buffer(size_t size);
+    int_buffer(const int* source, size_t size);
+    int_buffer(const int_buffer& rhs);
+    int_buffer(int_buffer&& rhs);
+    int_buffer & operator=(const int_buffer& rhs);
+    int_buffer & operator=(int_buffer&& rhs);
+    size_t size() const;
+    int* begin();
+    int* end();
+    const int* begin() const;
+    const int* end() const;
+    ~int_buffer();
+};
+
+class int_sorted{
+    int_buffer buffer;
+public:
+    int_sorted(const int* source, size_t size);
+    size_t size() const;
+    int* insert(int value);
+    const int* begin() const;
+    const int* end() const;
+    int_sorted merge(const int_sorted& merge_with) const;
+};
+
+#endif /* INT_BUFFER_H */
+
