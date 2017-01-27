b934dd6ac8e9ecbfa3ba84f4be250a0c83f8e80d
stefa_000 <stefa_000@10.250.118.98>
2017-01-27 17:17:29 +0100

    Implementerade merge men vet inte om fungerar.

--- int_sorted.cpp
+++ int_sorted.cpp
@@ -3,50 +3,76 @@
 #include <algorithm>
 #include <iostream>
 
-int_sorted::int_sorted(){
-    
+int_sorted::int_sorted() {
+
 }
 
-int_sorted::int_sorted(const int* source, size_t size){//Varför denna ej fungera?
+int_sorted::int_sorted(const int* source, size_t size) {//Varför denna ej fungera?
     this->buffer = new int_buffer(source, size);
 }
 
 
 /*:
-buffer(new int_buffer(source, size))*/ 
+buffer(new int_buffer(source, size))*/
 
 //size_t int_sorted::size(){
-    //return buffer.size();
+//return buffer.size();
 //}
 
-int* int_sorted::insert(int value){
+int* int_sorted::insert(int value) {
     /*size_t count = 1;
     int* i = buffer.begin();
     for(; i != buffer.end() && i != nullptr; i++){
         count++;
     }
     if(buffer.size() != count){
-        *i = value;
+     *i = value;
     }
     else{
         buffer = new int_buffer(buffer.begin(), buffer.size() + 10);
     }*/
     /*int_buffer tempbuffer(buffer.begin(), buffer.size() + 1);
     buffer = tempbuffer;
-    *(buffer.end()) = value;
+     *(buffer.end()) = value;
     return buffer.end();*/
 }
 
-const int* int_sorted::begin() const{
+const int* int_sorted::begin() const {
     return (*buffer).begin();
 }
 
-const int* int_sorted::end() const{
+const int* int_sorted::end() const {
     return (*buffer).end();
 }
 
-int_sorted int_sorted::merge(const int_sorted& merge_with) const{
+int_sorted int_sorted::merge(const int_sorted& merge_with) const {
     size_t newsize = buffer->size() + merge_with.buffer->size();
+    int* i = (*buffer).begin();
+    int* j = merge_with.buffer->begin();
+    int* newarray = new int[newsize];
+    size_t count = 0;
+    while (i != (*buffer).end() && j != merge_with.buffer->end()) {
+        if (*i > *j) {
+            newarray[count] = *j;
+            j++;
+            count++;
+        } else {
+            newarray[count] = *i;
+            i++;
+            count++;
+        }
+    }
+    while (i != (*buffer).end()) {
+        newarray[count] = *i;
+        i++;
+        count++;
+    }
+    while (j != merge_with.buffer->end()) {
+        newarray[count] = *j;
+        j++;
+        count++;
+    }
+    /*size_t newsize = buffer->size() + merge_with.buffer->size();
     int* newarray = new int[newsize];
     size_t count = 0;
     for(int* i = (*buffer).begin(); i != (*buffer).end(); i++){
@@ -56,7 +82,7 @@ int_sorted int_sorted::merge(const int_sorted& merge_with) const{
     for(int* i = merge_with.buffer->begin(); i != merge_with.buffer->end(); i++){
         newarray[count] = *i;
         count++;
-    }
+    }*/
     //this->buffer = new int_buffer(buffer->begin(), newsize);
     int_sorted* newsorted = new int_sorted(newarray, newsize);
     return *newsorted;
--- main.cpp
+++ main.cpp
@@ -5,22 +5,39 @@
 
 using namespace std;
 
+size_t number;
+const int* starter;
+const int* ender;
+
 int_sorted sort(const int* begin, const int* end){
     if(begin == end) return int_sorted();
     if(begin == end - 1) return int_sorted(begin, 1);
     ptrdiff_t half = (end-begin)/2;
     const int* mid = begin + half;
+    cout << "round: " << number << endl;
+    number++;
+    cout << "begin: " << *begin << endl;
+    cout << "mid: " << *mid << endl;
+    cout << "end: " << *end << endl;
+    cout << "Currently in the array: "; 
+    for(const int* i = starter; i != ender; i++){
+        cout << *i << ", ";
+    }
+    cout << endl;
     return sort(begin, mid).merge(sort(mid, end));
 }
 
 void f(int_buffer buf) {
+    number = 0;
     *buf.begin() = 1;
     for (int* i = buf.begin(); i != buf.end(); i++)
     {
         *(i + 1) = *i + 1;
     }
     const int temparray[10] = {3, 40, 20, 21 ,10 , 11, 23, 22, 12, 33};
-    
+    starter = temparray;
+    ender = temparray + 10;
+    cout << *ender << endl;
     sort( temparray, (temparray + 10));
  }
 
